1b6706e12611ce693d57ab19a6829c39be6ed033
Jonathan Reardon <jonathanjreardon@gmail.com>
2019-11-30 22:10:30 +0000

    added comments

--- Reminding_Ads.py
+++ Reminding_Ads.py
@@ -21,33 +21,37 @@ dataFile = open(current_time +'.csv', 'w')
 writer = csv.writer(dataFile)
 writer.writerow(["participant number", "video name"])
 
-win.fullscr = False           #not sure if this is necessary
+# minimise window in order to take user info
+win.fullscr = False #not sure if this is necessary
 win.winHandle.set_fullscreen(False)
 win.winHandle.minimize()
 win.flip()
 
-# Participant naming 
+# take user info
 expInfo = {'participant': ''}
 dlg = gui.DlgFromDict(dictionary=expInfo)
 if dlg.OK == False:
     core.quit()
     
+# maximise window to continue
 win.winHandle.maximize()
 win.winHandle.activate()
 win.fullscr=True
 win.winHandle.set_fullscreen(True)
 win.flip()
 
+# set ad video directory
 ads   = r"C:\Users\Channel 4 Project\Documents\C4_pilot\Reminding\Trialfolders\Advert"
 ad_videos = sorted(os.listdir(ads))
 #random.shuffle(ad_videos)
 
+# write participant code and video stim order to file before we begin exp
 for counter in range(len(ad_videos)):
     dataFile.write('%s, %s\n'%(expInfo.get('participant'), ad_videos[counter]))
 dataFile.close()
 
+# function to make and display intro stimuli and wait for user keypress to continue
 def intro():
-    
     intro_stim = visual.TextStim(win, text=r"Watch as you would at home", pos=(0,0), depth=0, wrapWidth=None, 
             color=("white"), opacity=1.0, contrast=1.0, units='deg', ori=0.0, height=3, antialias=True)
 
@@ -56,11 +60,13 @@ def intro():
         intro_stim.draw()
         win.flip()
         
+# create experiment stimuli
 question_stim = visual.TextStim(win, text=r"Did the object repeat? 'A' key = no, 'L' key = yes", pos=(0,0), depth=0, wrapWidth=None, 
             color=("white"), opacity=1.0, contrast=1.0, units='deg', ori=0.0, height=3, antialias=True)
 verbal = visual.TextStim(win, text=r"Tell the experimenter what ad, spacebar to continue", pos=(0,0), depth=0, wrapWidth=None, 
             color=("white"), opacity=1.0, contrast=1.0, units='deg', ori=0.0, height=3, antialias=True)
  
+ # function to make and display outro stimuli and wait for keypress to continue
 def outro():
     outro_stim = visual.TextStim(win, text=r"Thank you", pos=(0,0), depth=0, 
             color=("white"), opacity=1.0, contrast=1.0, units='deg', ori=0.0, height=3, antialias=True, wrapWidth=None)
@@ -70,11 +76,11 @@ def outro():
         outro_stim.draw()
         win.flip() 
         
+# display outro stim
 intro()
 
-
 for counter in range(len(ad_videos)):
-    
+    # create current video to display
     display = [visual.MovieStim3(win, ads + "/" + stim, size=[1920, 1080]) for stim in ad_videos[counter:counter + 1]]
 
     print(ad_videos[counter])
@@ -84,17 +90,19 @@ for counter in range(len(ad_videos)):
 
     shouldflip = display[0].play()
         
+    # while video is not finished, play it
     while display[0].status != visual.FINISHED:
         shouldflip = display[0].draw()
         win.flip()
-    
+    # check for keypress to quit 
     allKeys = event.getKeys(keyList = ('q'))
     for thisKey in allKeys:
         if thisKey == 'q':
             dataFile.close()
             win.close()
             core.quit()
-                
+
+# display outro stim         
 outro()
         
 win.close()
